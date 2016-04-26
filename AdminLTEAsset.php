00c491157cffd7e6263041e7c6eb5513bc0e6a1e
Memory Clutter <memclutter@gmail.com>
2016-04-26 11:08:04 +0300

    Check debug mode

--- AdminLTEAsset.php
+++ AdminLTEAsset.php
@@ -8,13 +8,25 @@ class AdminLTEAsset extends AssetBundle
 {
     public $sourcePath = '@bower/admin-lte/dist';
     public $baseUrl = '@web';
-    public $css = [
-        'css/AdminLTE.css',
-    ];
-    public $js = [
-        'js/app.min.js',
-    ];
     public $depends = [
         'yii\bootstrap\BootstrapPluginAsset',
     ];
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        $css = YII_DEBUG ? 'css/AdminLTE.css' : 'css/AdminLTE.min.css';
+        $this->css = [
+            $css
+        ];
+
+        $js = YII_DEBUG ? 'js/app.js' : 'js/app.min.js';
+        $this->js = [
+            $js
+        ];
+    }
 }
