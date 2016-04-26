829bb400c5071e9f45bcc116b3104270a1b16d9f
Memory Clutter <memclutter@gmail.com>
2016-04-26 10:27:30 +0300

    Depends on icheck

---
+++ AdminLTELoginAsset.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace memclutter\AdminLTE;
+
+use memclutter\ICheck\ICheckAsset;
+use yii\web\AssetBundle;
+
+class AdminLTELoginAsset extends AssetBundle
+{
+    public static $iCheckSkin;
+
+    public $depends = [
+        'memclutter\ICheck\ICheckAsset',
+    ];
+
+    public function init()
+    {
+        parent::init();
+
+        if (empty(self::$iCheckSkin)) {
+            self::$iCheckSkin = ICheckAsset::SKIN_ALL;
+        }
+
+        ICheckAsset::$skin = self::$iCheckSkin;
+    }
+}
